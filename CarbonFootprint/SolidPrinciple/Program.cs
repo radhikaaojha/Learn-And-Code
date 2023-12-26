6b9270771d10d972e61087582477b39ce31829c7
Radhika Ojha <radhika.ojha@intimetec.com>
2023-12-26 12:29:17 +0530

    SRP

---
+++ CarbonFootprint/SolidPrinciple/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace SolidPrinciple.SRP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(5.0, 3.0);
+            double area = rectangle.CalculateArea();
+
+            Display display = new Display();
+            display.DisplayArea(area);
+        }
+    }
+}
