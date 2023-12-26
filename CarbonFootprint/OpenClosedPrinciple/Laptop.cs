49dc3664f28ed9dd36942e8250c0ac7af6ea3fb1
Radhika Ojha <radhika.ojha@intimetec.com>
2023-12-26 15:03:06 +0530

    OCP

---
+++ CarbonFootprint/OpenClosedPrinciple/Laptop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrinciple
+{
+    public class Laptop : IDevice
+    {
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Laptop: Model Y, RAM: 16GB, Storage: 512GB");
+        }
+    }
+}
---
+++ CarbonFootprint/OpenClosedPrinciple/SmartPhone.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenClosedPrinciple
+{
+    public class SmartPhone : IDevice
+    {
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Smartphone: Model X, RAM: 8GB, Storage: 128GB");
+        }
+    }
+}
